68a90596053a66e9be11518c833f8fb3e196e9a4
Wenlin Mao <32403824+WenlinMao@users.noreply.github.com>
2020-05-03 14:24:12 -0700

    Gameplay (#48)
    * Staging (#29)
    
    * add screen shot
    
    * Graphics (#23)
    
    * Finish api (#9)
    
    * add doc dir test
    
    * add more
    
    * reset
    
    * test
    
    * test
    
    * add start structure
    
    * add starter structure
    
    * initialize project with xcode
    
    * edit output paths & gitignore
    
    * remove redundant reference in xcode
    
    * test
    
    * add imgui and files
    
    * Delete .DS_Store
    
    * add screen shot
    
    Co-authored-by: Cain <cainzhou96@gmail.com>
    
    * move imgui to lib
    
    * refactoring and a bug
    
    * bug fix
    
    * put window into client
    
    * add sphere class
    
    * add sphere, merge together
    
    * add sphere
    
    * add sphere
    
    * test for slack
    
    * add sphere and merge
    
    * resolve some merge issue
    
    * remove imgui from src
    
    Co-authored-by: Cain <cainzhou96@gmail.com>
    
    * Integrate network and graphic
    
    * Synced with network group
    
    * Synced with Network
    
    * bug
    
    * update io data format
    
    * Staging (#39)
    
    * Graphics (#16)
    
    * Finish api (#9)
    
    * add doc dir test
    
    * add more
    
    * reset
    
    * test
    
    * test
    
    * initialize project with xcode
    
    * edit output paths & gitignore
    
    * remove redundant reference in xcode
    
    * test
    
    * add imgui and files
    
    * Delete .DS_Store
    
    Co-authored-by: Wenlin Mao <32403824+WenlinMao@users.noreply.github.com>
    Co-authored-by: Cain <cainzhou96@gmail.com>
    Co-authored-by: Wenlin Mao <w6mao@ucsd.edu>
    
    * Graphics (#22)
    
    * Finish api (#9)
    
    * add doc dir test
    
    * add more
    
    * reset
    
    * test
    
    * test
    
    * initialize project with xcode
    
    * edit output paths & gitignore
    
    * remove redundant reference in xcode
    
    * test
    
    * add imgui and files
    
    * Delete .DS_Store
    
    * add screen shot
    
    Co-authored-by: Cain <cainzhou96@gmail.com>
    
    * merge
    
    * Gameplay (#38)
    
    * Staging (#29)
    
    * add screen shot
    
    * Graphics (#23)
    
    * Finish api (#9)
    
    * add doc dir test
    
    * add more
    
    * reset
    
    * test
    
    * test
    
    * add start structure
    
    * add starter structure
    
    * initialize project with xcode
    
    * edit output paths & gitignore
    
    * remove redundant reference in xcode
    
    * test
    
    * add imgui and files
    
    * Delete .DS_Store
    
    * add screen shot
    
    Co-authored-by: Cain <cainzhou96@gmail.com>
    
    * move imgui to lib
    
    * refactoring and a bug
    
    * bug fix
    
    * put window into client
    
    * add sphere class
    
    * add sphere, merge together
    
    * add sphere
    
    * add sphere
    
    * test for slack
    
    * add sphere and merge
    
    * resolve some merge issue
    
    * remove imgui from src
    
    Co-authored-by: Cain <cainzhou96@gmail.com>
    
    * Integrate network and graphic
    
    * Synced with network group
    
    * Synced with Network
    
    * bug
    
    * update io data format
    
    Co-authored-by: Wenlin Mao <32403824+WenlinMao@users.noreply.github.com>
    Co-authored-by: Cain <cainzhou96@gmail.com>
    Co-authored-by: IreneChen <yic328@ucsd.edu>
    
    Co-authored-by: peizhenwu <32442789+peizhenwu@users.noreply.github.com>
    Co-authored-by: Cain <cainzhou96@gmail.com>
    Co-authored-by: Azusa <32504776+ziw261@users.noreply.github.com>
    Co-authored-by: IreneChen <yic328@ucsd.edu>
    
    * Solve bugs and created new ones
    
    Bug1: new move with mat4 as parameter does not support two clients.
    Bug2: the flashing problem is kinda obvious
    
    * add screen shot
    
    * a simple camera
    
    * change camera control scheme
    
    * bug fix when camera too close to up vector
    
    * Tuning Camera
    
    * Mouse Input Basic Detection
    
    * mouse left or right
    
    * support mouse drag
    
    * Minor Bug Fixed
    
    * Bug fixed, client send to server successfully
    
    * network side bug fixed
    
    * camera added for different players
    
    * small bug fixed
    
    Co-authored-by: Cain <cainzhou96@gmail.com>
    Co-authored-by: IreneChen <yic328@ucsd.edu>
    Co-authored-by: ziw261 <ziw261@ucsd.edu>
    Co-authored-by: peizhenwu <32442789+peizhenwu@users.noreply.github.com>
    Co-authored-by: Azusa <32504776+ziw261@users.noreply.github.com>

--- server/server.cpp
+++ server/server.cpp
@@ -43,7 +43,7 @@ private:
     void send_info(int id, std::shared_ptr<tcp::socket> socket){
         while(1){
             std::string msg = obj.data()+'\n';
-            std::cout << msg ;
+            //std::cout << msg ;
             boost::asio::write( *socket, boost::asio::buffer(msg) );
             std::this_thread::sleep_for(std::chrono::milliseconds(30));
         }
@@ -56,7 +56,8 @@ private:
             boost::asio::read_until( *socket, buf, "\n" );
             std::string data = boost::asio::buffer_cast<const char*>(buf.data());
             std::string key_op = "";
-            std::string mouse_op;
+            std::string mouse_op = "";
+            float temp[4];
             
             // Read JSON from client
             try{
@@ -71,15 +72,27 @@ private:
                     BOOST_FOREACH(const pt::ptree::value_type& child, tar.get_child("cmd")) {
                         if(i == 0){
                             key_op = child.second.get<std::string>("key");
-                            cout << key_op << endl;
                         }
                         else{
-                            // Mouse cmd
+                            // Mouse
+                            mouse_op = child.second.get<std::string>("mouse");
+                            if(mouse_op.compare("l")){
+                                int index = 0;
+                                BOOST_FOREACH(const pt::ptree::value_type& t, child.second.get_child("mouse_l")){
+                                    temp[index] = stof(t.second.data());
+                                    index++;
+                                }
+                            } else if(mouse_op.compare("r")){
+                                int index = 0;
+                                BOOST_FOREACH(const pt::ptree::value_type& t, child.second.get_child("mouse_r")){
+                                    temp[index] = stof(t.second.data());
+                                    index++;
+                                }
+                            }
+                            
                         }
                         i++;
                     }
-                    
-      
                 }
             } catch (...){
                     
@@ -92,6 +105,11 @@ private:
                 }else if(id == 2){
                     obj.update2(key_op.at(0));
                 }
+            }
+            if(mouse_op != ""){
+                //cout << "id: " << id << ", button: " << mouse_op <<endl;
+                cout << mouse_op << ": " << temp[0] << ", " << temp[1] << ", " << temp[2] << ", " << temp[3] << endl;
+                
             }
         }
     }
